811e70e9aec0e265c69ef3ff5d972b2b969cf46b
Sharath N <166585965+sharathnandan@users.noreply.github.com>
2024-05-28 15:53:55 -0400

    Add files via upload

---
+++ flowsensorcode.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.print("The flow rate at this point is currently ")
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
